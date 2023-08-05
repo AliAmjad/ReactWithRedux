e538d211636fca9d598e5f9316e15227fcb92431
aliamjad <aliamjad.dev@gmail.com>
2023-08-06 02:43:49 +0500

    Finalizing the app.

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,16 +1,20 @@
 import * as actionTypes from "./actionTypes";
 import * as courseApi from "../../api/courseApi";
 
-export function createCourse(course) {
-  return { type: actionTypes.CREATE_COURSE, course: course };
-}
-
 export function removeCourse(course) {
   return { type: actionTypes.REMOVE_COURSE, course: course };
 }
 
 export function loadCoursesSuccess(courses) {
   return { type: actionTypes.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function createCourseSuccess(courses) {
+  return { type: actionTypes.CREATE_COURSE_SUCCESS, courses };
+}
+
+export function updateCourseSuccess(courses) {
+  return { type: actionTypes.UPDATE_COURSE_SUCCESS, courses };
 }
 
 export function loadCourses() {
@@ -20,6 +24,21 @@ export function loadCourses() {
       .then((courses) => {
         // dispatching the success action
         dispatch(loadCoursesSuccess(courses));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export function saveCourse(course) {
+  return function (dispatch, getState) {
+    return courseApi
+      .saveCourse(course)
+      .then((savedCourse) => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse));
       })
       .catch((error) => {
         throw error;
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -3,11 +3,16 @@ import initialState from "./initialState";
 
 export default function courseReducer(state = initialState.courses, action) {
   switch (action.type) {
-    case actionTypes.CREATE_COURSE:
+    case actionTypes.CREATE_COURSE_SUCCESS:
       // step 2
       // debugger;
 
       return [...state, { ...action.course }];
+
+    case actionTypes.UPDATE_COURSE_SUCCESS:
+      return state.map((course) =>
+        course.id === action.course.id ? action.course : course
+      );
 
     case actionTypes.REMOVE_COURSE:
       // we are not mutating state as it'll return a new state
